821267487a0ef0a40e1918ca5e8a1e2edb36c074
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-07-15 21:11:33 +1000

    Add CL!

--- src/main/scala/gpu/Entry.scala
+++ src/main/scala/gpu/Entry.scala
@@ -9,4 +9,23 @@ object Main extends App {
     .emitVerilog(new GPU(), Array("--target-dir", "generated"))
 }
 
+// A control list is variable length. How to implement? sequential or stream based input?
+class ControlListExecutor extends Module {
+  val io = IO {
+    new Bundle {
+      val control_code = Bits(256.W)
+      // control list = variable length command
+      // next part => set of vertex attributes + primitives (triangles)
+      val control_list = UInt(32.W)
+    }
+  }
+
+  // use it to figure out what to do
+  io.control_list := 0
+}
+
+class DMAWriter extends Module {}
+
+class VertexCacher extends Module {}
+
 class GPU extends Module {}
